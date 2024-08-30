4c8ffc9df288838aa954c9311bd0be699a02f25b
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-30 09:00:41 -0300

    Update analise.c

--- analise.c
+++ analise.c
@@ -1,10 +1,11 @@
+
 #include <stdio.h>
 
 int main()
 
 {
-    for (int i= 0; i <= 10, i++;){
-        printf("%d\n", i);
+    for (int i = 0; i <= 10; i++){
+        printf("%d,", i);
     }
     
     return 0;
