3281b6748a7e6ca684b02bfbfda854be85a0095d
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-30 12:03:41 -0300

    programinha

--- analise.c
+++ analise.c
@@ -1,12 +1,10 @@
-
 #include <stdio.h>
 
-int main()
-
-{
-    for (int i = 0; i <= 10; i++){
-        printf("%d,", i);
+int main() {
+    for (int i = 0; i <= 100; i++){
+        if(i % 2 == 1)
+        { printf("%d,", i);
     }
-    
+    }
     return 0;
-}
+    }
